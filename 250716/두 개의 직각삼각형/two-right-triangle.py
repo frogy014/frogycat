9bfc6939d10c4afdfdf23b657c2ac572d08ee27e
frogy014 <60348513+frogy014@users.noreply.github.com>
2025-07-16 13:56:01 +0900

    Title: 두 개의 직각삼각형, Time: 45ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250716/두 개의 직각삼각형/two-right-triangle.py
@@ -0,0 +1,9 @@
+n=int(input())
+for i in range(n,0,-1):
+    for j in range(i):
+        print("*",end="")
+    for j in range((n-i)*2):
+        print(" ",end="")
+    for j in range(i):
+        print("*",end="")
+    print()
