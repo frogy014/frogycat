a12c88f823e55c5b35327affe1912053c2aa4b9d
frogy014 <60348513+frogy014@users.noreply.github.com>
2025-07-21 16:18:40 +0900

    Title: 배열 채우기, Time: 43ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250721/배열 채우기/filling-array.py
+++ 250721/배열 채우기/filling-array.py
@@ -1,14 +1,13 @@
 arr = input().split()
-arr = arr[::-1]
-if arr[0]=="0":
-    for i in arr[1:]:
-        if i!="0":
-            print(i,end=" ")
-        else:
+if arr[len(arr)-1]=="0":
+    for i in arr[len(arr)-2::-1]:
+        if i=="0":
             break
+        else:
+            print(i,end=" ")
 else:
-    for i in arr:
-        if i!="0":
-            print(i,end=" ")
-        else:
+    for i in arr[::-1]:
+        if i=="0":
             break
+        else:
+            print(i,end=" ")
