352c13bcf42b9ffa679d49ea663f3872d25a3ec9
frogy014 <60348513+frogy014@users.noreply.github.com>
2025-07-21 13:55:41 +0900

    Title: 특정 수 도달하기, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

--- 250721/특정 수 도달하기/reaching-specific-number.py
+++ 250721/특정 수 도달하기/reaching-specific-number.py
@@ -2,9 +2,8 @@ arr = map(int, input().split())
 sum_arr=0
 cnt=0
 for i in arr:
-    sum_arr+=i
-    if sum_arr>=250:
-        sum_arr-=i
+    if i>=250:
         break
+    sum_arr+=i
     cnt+=1
 print(f"{sum_arr} {sum_arr/cnt:.1f}")
