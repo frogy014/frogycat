60d8615f5809e098bdda1ce6612c5ef41ba19485
frogy014 <60348513+frogy014@users.noreply.github.com>
2025-07-22 16:29:48 +0900

    Title: 십의 자리 숫자, Time: 77ms, Memory: 24MB, Status: Runtime Error - Codetree

--- 250722/십의 자리 숫자/number-of-tens-digit.py
+++ 250722/십의 자리 숫자/number-of-tens-digit.py
@@ -1,6 +1,8 @@
 one_count = [0]*10
 arr= list(map(int,input().split()))
 for i in arr:
+    if i=0:
+        break
     one_count[i//10]+=1
 for i in range(1,10):
     print(f"{i} - {one_count[i]}")
