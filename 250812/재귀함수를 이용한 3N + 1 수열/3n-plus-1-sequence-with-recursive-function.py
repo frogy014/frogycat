cb569515f8a8b7d16b8cf18942ddf09662d5c623
frogy014 <60348513+frogy014@users.noreply.github.com>
2025-08-12 17:54:27 +0900

    Title: 재귀함수를 이용한 3N + 1 수열, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250812/재귀함수를 이용한 3N + 1 수열/3n-plus-1-sequence-with-recursive-function.py
@@ -0,0 +1,11 @@
+n = int(input())
+
+# Please write your code here.
+def cnt(n):
+    if n==1:
+        return 0
+    if n%2==0:
+        return cnt(n//2)+1
+    else:
+        return cnt(n*3+1)+1
+print(cnt(n))
