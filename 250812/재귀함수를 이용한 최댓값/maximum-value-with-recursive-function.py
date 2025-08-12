fd57f1a108f50ecc5531a9927ec67786589db041
frogy014 <60348513+frogy014@users.noreply.github.com>
2025-08-12 17:43:15 +0900

    Title: 재귀함수를 이용한 최댓값, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250812/재귀함수를 이용한 최댓값/maximum-value-with-recursive-function.py
@@ -0,0 +1,9 @@
+n = int(input())
+arr = list(map(int, input().split()))
+
+# Please write your code here.
+def max_arr(n):
+    if n==1:
+        return max(arr[1],arr[0])
+    return max(max_arr(n-1),arr[n-1])
+print(max_arr(n))
